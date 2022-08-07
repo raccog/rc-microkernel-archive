5d276472066eb3ce157c1c881183115b08a2a35a
Ryan Cohen <rcohenprogramming@gmail.com>
2022-08-07 15:28:05 -0400

    Add function for printing signed integer to serial.

--- Kernel/Serial.h
+++ Kernel/Serial.h
@@ -12,16 +12,35 @@ class Serial : public CharacterWriter {
     };
 
     ComPort m_port;
+    bool m_initialized;
 
     int init();
     bool is_transmit_empty();
 
 public:
-    Serial() : m_port{COM1} {}
-    Serial(ComPort port) : m_port{port} {}
+    Serial() : m_port{COM1}, m_initialized{false} {}
+    Serial(ComPort port) : m_port{port}, m_initialized{false} {}
 
-    static Serial init_default();
+    static Serial &init_default();
 
     int write(const char *str);
     int write_char(char c);
 };
+
+/* Temporary function
+ *
+ * Print signed integer to serial output.
+ */
+void kserial_int(i64 value);
+
+/* Temporary function
+ *
+ * Print unsigned integer to serial output.
+ */
+void kserial_uint(u64 value);
+
+/* Temporary function
+ *
+ * Print hex integer to serial output.
+ */
+void kserial_hex(u64 value);
--- Kernel/Start.cpp
+++ Kernel/Start.cpp
@@ -7,6 +7,8 @@
 #include "Segmentation.h"
 #include "Serial.h"
 
+#include <limits.h>
+
 constexpr const char *KERNEL_VERSION = "v0.0.1";
 
 [[noreturn]] static void done() {
@@ -21,11 +23,16 @@ constexpr const char *KERNEL_VERSION = "v0.0.1";
 extern "C" void _start() {
     // init limine terminal
     LimineTerminal terminal = LimineTerminal::init_default();
-    terminal.write("Hello limine terminal\nend");
+    terminal.write("Hello limine terminal\nend\n");
 
     // init serial device
-    Serial serial = Serial::init_default();
-    serial.write("Hello serial\nend");
+    Serial &serial = Serial::init_default();
+    serial.write("Hello serial\nend\n");
+    kserial_int(-1234567890);
+    kserial_int(INT_MIN);
+    kserial_int(LLONG_MIN);
+    kserial_int(LLONG_MAX);
+    kserial_int(0);
 
     // Halt
     done();
