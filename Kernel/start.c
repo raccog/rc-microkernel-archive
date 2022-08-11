5bf2072c3b46fcbdea1094683866a1978aa4d3f8
Ryan Cohen <rcohenprogramming@gmail.com>
2022-08-11 16:22:00 -0400

    Separate _start() into initialization phases.

--- Kernel/start.c
+++ Kernel/start.c
@@ -6,34 +6,59 @@
 #include "serial.h"
 #include "terminal.h"
 
-#include <limits.h>
-
 #define KERNEL_VERSION "v0.0.1"
 #define BOOT_MESSAGE                                                           \
     "rc-microkernel version (" KERNEL_VERSION ") compiled on " __DATE__        \
     " at " __TIME__ "\n"
 
-/**
- * Kernel start function.
+/*
+ * Initialize architecture-dependent character output devices.
+ *
+ * This is done right away so that logging can be done as early in the boot
+ * process as possible.
  */
-void _start() {
+static void arch_early_init() {
     /* Initialize 2 character output devices */
     terminal_init();
     serial_init();
     fallback_chardev_init();
+}
 
-    /*
-     * NOTE
-     * printk() and debugk() can ONLY be used after this line.
-     */
-
-    /* Print boot message */
+/*
+ * Print a boot message to stdout.
+ */
+static void print_boot_message() {
     printk(BOOT_MESSAGE);
     /* Only print to stddebug if it is a separate device from stdout */
     if (stdout.write != stddebug.write) {
         debugk(BOOT_MESSAGE);
     }
+}
 
-    // Halt
+/*
+ * Initialize architecture-dependent features that are necessary for kernel
+ * initalization.
+ */
+static void arch_init() {
+    return;
+}
+
+/**
+ * Kernel start function.
+ */
+void _start() {
+    arch_early_init();
+
+    /*
+     * NOTE
+     * printk() and debugk() can ONLY be used after arch_early_init() is
+     * called.
+     */
+
+    print_boot_message();
+
+    arch_init();
+
+    /* TODO: Start first process */
     panic();
 }
