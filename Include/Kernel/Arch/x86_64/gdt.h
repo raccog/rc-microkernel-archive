2f4655245127ba632f535422c975a7dda1976ec1
Ryan Cohen <rcohenprogramming@gmail.com>
2022-08-15 15:04:33 -0400

    Update GDT descriptor struct to use fixed-width integer types.

--- Include/Kernel/Arch/x86_64/gdt.h
+++ Include/Kernel/Arch/x86_64/gdt.h
@@ -14,17 +14,17 @@
  * A descriptor for a memory segment in the GDT.
  */
 struct mem_segment_descriptor {
-    unsigned int lolimit : 16;  /* segment extent (lsb) */
-    unsigned int lobase : 24;   /* segment base address (lsb) */
-    unsigned int sd_type : 5;   /* segment type */
-    unsigned int dpl : 2;       /* segment descriptor priority level */
-    unsigned int p : 1;         /* segment descriptor present */
-    unsigned int hilimit : 4;   /* segment extent (msb) */
-    unsigned int available : 1; /* available */
-    unsigned int long_mode : 1; /* long mode */
-    unsigned int def32 : 1;     /* default 32 vs 16 bit size */
-    unsigned int gran : 1;      /* limit granularity (byte/page) */
-    unsigned int hibase : 8;    /* segment base address (msb) */
+    u16 lolimit;      /* segment extent (lsb) */
+    u32 lobase : 24;  /* segment base address (lsb) */
+    u32 sd_type : 5;  /* segment type */
+    u32 dpl : 2;      /* segment descriptor privilege level */
+    u32 present : 1;  /* segment descriptor present */
+    u8 hilimit : 4;   /* segment extent (msb) */
+    u8 available : 1; /* available */
+    u8 long_mode : 1; /* long mode */
+    u8 def32 : 1;     /* default 32 vs 16 bit size */
+    u8 gran : 1;      /* limit granularity (byte/page) */
+    u8 hibase;        /* segment base address (msb) */
 } __attribute__((packed));
 
 /**
